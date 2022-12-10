8bbe59154a5845aa44464b7bf65d04805458d1a2
abreuthrj <abreuthrj@gmail.com>
2022-12-10 18:31:54 -0300

    added responsivity

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,68 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { CustomContainer, CustomItem } from "./styles";
+import { IconButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { MdMenu } from "react-icons/md";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Background, CustomContainer, CustomItem, MenuIcon } from "./styles";
 
 const Menu: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const [mobile, setMobile] = useState(false);
+  const [mobileVisible, setMobileVisible] = useState(false);
+
+  useEffect(() => {
+    const eventListener = () => {
+      if (window.innerWidth > 400) {
+        setMobile(false);
+        setMobileVisible(false);
+      } else {
+        setMobile(true);
+      }
+    };
+    window.addEventListener("resize", eventListener);
+
+    eventListener();
+
+    return () => {
+      window.removeEventListener("resize", eventListener);
+    };
+  }, []);
+
+  useEffect(() => {
+    setMobileVisible(false);
+  }, [location]);
 
   const handleLogout = () => {
     navigate("/login");
   };
 
   return (
-    <CustomContainer>
-      <CustomItem onClick={() => navigate("/")}>Home</CustomItem>
-      <CustomItem onClick={() => navigate("/donations")}>Doações</CustomItem>
-      <CustomItem onClick={() => navigate("/new-donation")}>
-        Cadastrar doação
-      </CustomItem>
-      <CustomItem onClick={handleLogout}>Logout</CustomItem>
-    </CustomContainer>
+    <>
+      <MenuIcon>
+        <IconButton
+          onClick={() => setMobileVisible(true)}
+          sx={{ bgcolor: "#B000A430" }}
+        >
+          <MdMenu color="white" />
+        </IconButton>
+      </MenuIcon>
+
+      {mobileVisible && <Background onClick={() => setMobileVisible(false)} />}
+
+      {((mobile && mobileVisible) || !mobile) && (
+        <CustomContainer mobileVisible={mobileVisible}>
+          <CustomItem onClick={() => navigate("/")}>Home</CustomItem>
+          <CustomItem onClick={() => navigate("/donations")}>
+            Doações
+          </CustomItem>
+          <CustomItem onClick={() => navigate("/new-donation")}>
+            Cadastrar doação
+          </CustomItem>
+          <CustomItem onClick={handleLogout}>Logout</CustomItem>
+        </CustomContainer>
+      )}
+    </>
   );
 };
 
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,11 +1,24 @@
 import styled from "@emotion/styled";
 import { MenuItem } from "@mui/material";
 
-export const CustomContainer = styled.div`
+export const CustomContainer = styled.div<{ mobileVisible?: boolean }>`
   width: 400px;
   height: 100%;
 
   background-color: purple;
+
+  transition: 200ms;
+
+  @media screen and (max-width: 400px) {
+    position: fixed;
+    z-index: 10;
+    width: ${(props) => (props.mobileVisible ? "80%" : "0%")};
+    opacity: ${(props) => (props.mobileVisible ? "1" : "0")};
+
+    * {
+      display: ${(props) => (props.mobileVisible ? "flex" : "none")};
+    }
+  }
 `;
 
 export const CustomItem = styled(MenuItem)((props) => ({
@@ -14,3 +27,27 @@ export const CustomItem = styled(MenuItem)((props) => ({
   fontWeight: 500,
   padding: "24px 48px",
 }));
+
+export const Background = styled.div`
+  width: 100%;
+  height: 100%;
+
+  position: fixed;
+  z-index: 9;
+
+  background-color: #00000060;
+`;
+
+export const MenuIcon = styled.div`
+  display: none;
+  position: fixed;
+
+  top: 0px;
+  left: 0px;
+
+  margin: 10px;
+
+  @media screen and (max-width: 400px) {
+    display: block;
+  }
+`;
